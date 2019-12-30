7e6b4b13c9539f81583552b77759e0fdd65cc7dc
Phillip Goldfarb <goldfarbphillip@gmail.com>
2019-12-30 07:37:05 +0000

    changed prod config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,10 +3,10 @@ set :rails_env, "production"
 set :delayed_job_workers, 12
 append :linked_files, ".env.production"
 
-server "kagamihara", :roles => %w(web app cron), :primary => true
-server "shima", :roles => %w(web app)
-server "saitou", :roles => %w(web app)
-server "oogaki", :roles => %w(worker)
+server "wtf.dev:1337", :roles => %w(web app cron worker), :primary => true
+#server "shima", :roles => %w(web app)
+#server "saitou", :roles => %w(web app)
+#server "oogaki", :roles => %w(worker)
 
 set :newrelic_appname, "Danbooru"
 after "deploy:finished", "newrelic:notice_deployment"
