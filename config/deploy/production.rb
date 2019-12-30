7b8cced7b28d896d69758bf0a96c106615aedaf2
Phillip Goldfarb <goldfarbphillip@gmail.com>
2019-12-30 07:41:10 +0000

    changed prod config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :rails_env, "production"
 set :delayed_job_workers, 12
 append :linked_files, ".env.production"
 
-server "wtf.dev:1337", :roles => %w(web app cron worker), :primary => true
+server "msv.wtf.dev", :roles => %w(web app cron), :primary => true
 #server "shima", :roles => %w(web app)
 #server "saitou", :roles => %w(web app)
 #server "oogaki", :roles => %w(worker)
